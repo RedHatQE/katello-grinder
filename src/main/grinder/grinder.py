c587ff491332e392c978197152395b9bf764a5ce
Brad P. Crochet <brad@redhat.com>
2012-04-18 14:49:28 -0400

    POC for running a testng test

--- src/main/grinder/grinder.py
+++ src/main/grinder/grinder.py
@@ -2,8 +2,14 @@ from java.lang import System
 from net.grinder.script.Grinder import grinder
 from net.grinder.script import Test
 from com.redhat.qe.perf.katello import SubscriptionManager
+from com.redhat.qe.katello.tests.api import A_ConsumersTest
+from org.testng import TestNG
+from org.testng import TestListenerAdapter
+from java.lang import Class
+from jarray import array
 
-test1 = Test(1, "Subscription Manager")
+#test1 = Test(1, "Subscription Manager")
+test2 = Test(1, "Consumer Test")
 
 props = System.getProperties()
 protocol = props['katello.protocol']
@@ -13,12 +19,22 @@ context = props['katello.context']
 username = props['katello.username']
 password = props['katello.password']
 orgToTest = props['katello.org']
-request1 = test1.record(SubscriptionManager(protocol, hostname, port, context, username, password))
+#request1 = test1.record(SubscriptionManager(protocol, hostname, port, context, username, password))
+
+def testngRun():
+    tla = TestListenerAdapter()
+    testng = TestNG()
+    testng.setTestClasses(array([ A_ConsumersTest ], Class))
+    testng.addListener(tla)
+    testng.run()
+
+test2.record(testngRun)
 
 class TestRunner:
     def __call__(self):
-        result = request1.registerTo(orgToTest)
-        writeToFile(result)
+        #result = request1.registerTo(orgToTest)
+        #writeToFile(result)
+        testngRun()
 
 def writeToFile(text):
     filename = "%s-page-%d.html" % (grinder.processName, grinder.runNumber)
