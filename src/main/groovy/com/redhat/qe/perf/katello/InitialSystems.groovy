0f7d30060e191939e4c8bca9f5a14b0c2bf53db6
Brad P. Crochet <brad@redhat.com>
2013-02-14 07:33:46 -0500

    Make test org a system property

--- src/main/groovy/com/redhat/qe/perf/katello/InitialSystems.groovy
+++ src/main/groovy/com/redhat/qe/perf/katello/InitialSystems.groovy
@@ -22,11 +22,12 @@ class InitialSystems {
     def createInitialSystems() {
         KatelloTasks tasks = injector.getInstance(Key.get(KatelloTasks.class, PlainSSLContext.class))
         Integer systemCount = System.properties['katello.test.initialSystems'] as Integer
+        def testOrganization = System.properties['katello.test.organization'] as String ?: "ACME_Corporation"
         systemCount.times {
             def pid = KatelloUtils.getUniqueID()
             def consumer_name = "auto-"+pid+".example.com"
             def hostuuid = KatelloUtils.getUUID()
-            KatelloSystem consumer = tasks.createConsumer("ACME_Corporation", consumer_name, hostuuid)
+            KatelloSystem consumer = tasks.createConsumer(testOrganization, consumer_name, hostuuid)
             KatelloPemThreadLocal.set(consumer.idCert.cert + consumer.idCert.key)            
             KatelloTasks tasksWithCert = injector.getInstance(Key.get(KatelloTasks.class, CertSSLContext.class))
             def result = tasksWithCert.updatePackages(consumer)
